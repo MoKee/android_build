7c607f4b804c1b60475d7c9a90d2d381141c9aba
martincz <martincz@mokeetek.com>
2013-03-26 18:02:04 +0800

    build: path change
    Change-Id: Ibd56b5de291e44dd83ee859494b0b9d6da7dfcf6

--- envsetup.sh
+++ envsetup.sh
@@ -268,7 +268,7 @@ function addcompletions()
         return
     fi
 
-    dirs="sdk/bash_completion vendor/cm/bash_completion"
+    dirs="sdk/bash_completion vendor/mk/bash_completion"
     for dir in $dirs; do
     if [ -d ${dir} ]; then
         for f in `/bin/ls ${dir}/[a-z]*.bash 2> /dev/null`; do
