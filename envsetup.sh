bc3e34612c896c73ae5bcdab21868024fdf90614
Ryuinferno <ryuinferno.xda@gmail.com>
2014-07-08 19:20:25 +1000

    build: Alternative changelog tool
    Change-Id: I86fa9ed4c1577b82bb7828107b2064e6f8de0845

--- envsetup.sh
+++ envsetup.sh
@@ -30,7 +30,7 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - repopick: Utility to fetch changes from Gerrit.
 - installboot: Installs a boot.img to the connected device.
 - installrecovery: Installs a recovery.img to the connected device.
-- clog:     Tool to generate changelog.
+- chglog:     Tool to generate changelog.
 - ota_all:  Generate OTA packages using the MoKee OTA system.
 - translate: Tool to to aid in translating strings
 
@@ -2165,17 +2165,22 @@ unset f
 
 # Changelog Tool
 function clog() {
-    ./build/clog $1
+    $ANDROID_BUILD_TOP/build/clog $1
 }
 
 # OTA Script
 function ota_all() {
-    ./build/tools/mk_ota_script/gen_ota $1 $2
+    $ANDROID_BUILD_TOP/build/tools/mk_ota_script/gen_ota $1 $2
 }
 
 # Translation Tool
 function translate() {
     $ANDROID_BUILD_TOP/build/tools/translate.py $1 $2 $3
+}
+
+# Alternative Changelog Tool
+function chglog() {
+    $ANDROID_BUILD_TOP/build/tools/chglog.py $ANDROID_BUILD_TOP $1 $2
 }
 
 # Add completions
