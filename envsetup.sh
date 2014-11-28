7a25996daf3ad79b36170061b17912199ca66e85
martincz <martincz@mokeedev.com>
2014-11-29 02:32:10 +0800

    build: tmp disable pngcrusher
    Change-Id: Ib2cd6cdf4428e339484edc0fccd9e833997f78d3

--- envsetup.sh
+++ envsetup.sh
@@ -663,11 +663,6 @@ function lunch()
         fi
     fi
 
-    if [ "$(which pngquant)" == "" ]
-    then
-        echo -e "\033[1;33;41mpngquant is not installed! Builds will be larger!\033[0m"
-    fi
-
     export TARGET_PRODUCT=$product
     export TARGET_BUILD_VARIANT=$variant
     export TARGET_BUILD_TYPE=release
