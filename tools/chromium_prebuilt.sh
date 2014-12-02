712a70189aaeb90b0024cf79961cee998924f83c
Ryuinferno <ryuinferno.xda@gmail.com>
2014-12-02 18:59:16 +0100

    build: Add chromium prebuilt support (1/2)
    Ported from https://github.com/MoKee/android_build/commit/ee7cc925d405815db3d2a712941e3213f308ea1e
    Different folders to distingush between odexed and deodexed builds
    export USE_PREBUILT_CHROMIUM=1 to enable
    
    Change-Id: I2478d83e6bc4ba60971a85db549095f7bd005139

---
+++ tools/chromium_prebuilt.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+# Copyright (C) 2014 The MoKee OpenSource Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This works, but there has to be a better way of reliably getting the root build directory...
+if [ $# -eq 1 ]; then
+    TOP=$1
+    DEVICE=$TARGET_DEVICE
+elif [ -n "$(gettop)" ]; then
+    TOP=$(gettop)
+    DEVICE=$(get_build_var TARGET_DEVICE)
+else
+    echo "Please run envsetup.sh and lunch before running this script,"
+    echo "or provide the build root directory as the first parameter."
+    return 1
+fi
+
+TARGET_DIR=$OUT
+if [ -z "$MK_RELEASE" ]; then
+    PREBUILT_DIR=$TOP/prebuilts/chromium/$DEVICE/deodex
+    BUILD=deodex
+else
+    PREBUILT_DIR=$TOP/prebuilts/chromium/$DEVICE/odex
+    BUILD=odex
+fi
+
+if [ -d $PREBUILT_DIR ]; then
+    rm -rf $PREBUILT_DIR
+fi
+
+mkdir -p $PREBUILT_DIR
+mkdir -p $PREBUILT_DIR/webview
+mkdir -p $PREBUILT_DIR/lib
+
+if [ -d $TARGET_DIR ]; then
+    echo "Copying files..."
+    cp -rd $TARGET_DIR/system/app/webview $PREBUILT_DIR/webview/
+    cp $TARGET_DIR/system/lib/libwebviewchromium.so $PREBUILT_DIR/lib/libwebviewchromium.so
+else
+    echo "Please ensure that you have ran a full build prior to running this script!"
+    return 1;
+fi
+
+echo "Generating Makefiles..."
+
+HASH=$(git --git-dir=$TOP/external/chromium_org/.git --work-tree=$TOP/external/chromium_org rev-parse --verify HEAD)
+echo $HASH > $PREBUILT_DIR/hash.txt
+
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g | sed s/__BUILD__/$BUILD/g > $PREBUILT_DIR/chromium_prebuilt.mk
+# Copyright (C) 2014 The MoKee OpenSource Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := prebuilts/chromium/__DEVICE__/__BUILD__
+
+PRODUCT_COPY_FILES += \\
+    \$(call find-copy-subdir-files,*,$(LOCAL_PATH)/webview,system/app/webview) \\
+    \$(LOCAL_PATH)/lib/libwebviewchromium.so:system/lib/libwebviewchromium.so
+
+EOF
+
+echo "Done!"
