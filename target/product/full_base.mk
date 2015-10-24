2f35e25607782cc9c4cd78e54e0d881717853642
martincz <martincz@mokeedev.com>
2015-10-24 19:24:00 +0800

    build: disable audio config
    Change-Id: Ie5627128a9ee0406cac4995809d0e94bb6feef94

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -34,7 +34,7 @@ PRODUCT_LOCALES := zh_CN
 PRODUCT_AAPT_CONFIG := normal
 
 # Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+# $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 
 # Get the TTS language packs
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
