8b60fe0b0359c45c2b280b8ec22e00ed5422b077
martincz <martincz@mokeedev.com>
2016-09-20 00:41:13 +0800

    build: add clog commands
    Change-Id: I0701ce68f5423032ec8fb4c9c8dd8a5e57550c5c

--- envsetup.sh
+++ envsetup.sh
@@ -40,7 +40,9 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - installboot: Installs a boot.img to the connected device.
 - installrecovery: Installs a recovery.img to the connected device.
 - repodiff:  Diff 2 different branches or tags within the same repo
+- chglog:   Alternative tool to generate changelog.
 - clearout: Cleans out directory
+- clog:     Tool to generate changelog.
 
 Environment options:
 - SANITIZE_HOST: Set to 'true' to use ASAN for all host modules. Note that
@@ -2638,9 +2640,19 @@ check_bash_version && {
     done
 }
 
+# Alternative Changelog Tool
+function chglog() {
+    $ANDROID_BUILD_TOP/build/tools/chglog.py $ANDROID_BUILD_TOP $1 $2
+}
+
 # Cleans out directory
 function clearout() {
     rm -rf $ANDROID_BUILD_TOP/out/target/product
+}
+
+# Changelog Tool
+function clog() {
+    $ANDROID_BUILD_TOP/build/tools/clog $1
 }
 
 export ANDROID_BUILD_TOP=$(gettop)
