c1a66863806a4dd4199c36c7b3f2fa4ef741d9c9
martincz <martincz@mokeedev.com>
2017-08-22 14:41:04 +0800

    make: Put zh_CN first in the list, so make it default.
    Change-Id: I0433066a0a8170b810217011fea497f887eb5597

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -27,8 +27,8 @@ PRODUCT_PACKAGES := \
     libwnndict \
     WAPPushManager
 
-# Put en_US first in the list, so make it default.
-PRODUCT_LOCALES := en_US
+# Put zh_CN first in the list, so make it default.
+PRODUCT_LOCALES := zh_CN
 
 # Get some sounds
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
