f0f3c36b8903d0398c78d07d1352645ddc0b3358
martincz <martincz@mokeedev.com>
2017-08-24 01:11:22 +0800

    make: export MK_CPU_ABI
    Change-Id: I74e8402485a8eadbaeeaa10c6dad8e98d453cf68

--- envsetup.sh
+++ envsetup.sh
@@ -298,6 +298,8 @@ function setpaths()
     unset ANDROID_TARGET_OUT_TESTCASES
     export ANDROID_TARGET_OUT_TESTCASES=$(get_abs_build_var TARGET_OUT_TESTCASES)
 
+    export MK_CPU_ABI=$(get_build_var TARGET_CPU_ABI)
+
     # needed for building linux on MacOS
     # TODO: fix the path
     #export HOST_EXTRACFLAGS="-I "$T/system/kernel_headers/host_include
