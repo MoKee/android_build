9d16c1e0ebd550bf71764ea55202db7e2454efd1
martincz <martincz@mokeedev.com>
2017-08-26 08:14:46 +0800

    build: disable default audios
    Change-Id: Ie1f20c4c94b19096fcc82ae88bb744e7ed269fd7

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -46,7 +46,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
 PRODUCT_LOCALES := zh_CN
 
 # Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+# $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 
 # Get the TTS language packs
 # $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
