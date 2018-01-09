3de513d91ea188b2c4498539bc8041a339cef4f5
XiNGRZ <chenxingyu92@gmail.com>
2018-01-09 21:27:43 +0800

    build: make: Always use host bison
    Change-Id: Ief2cbab6d6059d6ffb8e898857a90344e5d1e583

--- core/config.mk
+++ core/config.mk
@@ -578,11 +578,7 @@ LEX := prebuilts/misc/$(BUILD_OS)-$(HOST_PREBUILT_ARCH)/flex/flex-2.5.39
 # To run bison from elsewhere you need to set up enviromental variable
 # BISON_PKGDATADIR.
 BISON_PKGDATADIR := $(PWD)/external/bison/data
-ifeq ($(USE_HOST_BISON),yes)
-BISON := bison
-else
-BISON := prebuilts/misc/$(BUILD_OS)-$(HOST_PREBUILT_ARCH)/bison/bison
-endif
+BISON := $(HOST_OUT_EXECUTABLES)/bison
 YACC := $(BISON) -d
 BISON_DATA := $(wildcard external/bison/data/* external/bison/data/*/*)
 
