443f03689ca9901849f78d8f0a812b8183fba06a
Ryuinferno <ryuinferno.xda@gmail.com>
2018-07-24 22:13:56 +0800

    build: Save updater-script in target files package
    Change-Id: Id476efce6b0358e9e5cd47dbcf48d7c956bbbed8

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -907,8 +907,14 @@ endif;
 
   script.SetProgress(1)
   script.AddToZip(input_zip, output_zip, input_path=OPTIONS.updater_binary)
+  input_zip_path = input_zip.filename
+  common.ZipClose(input_zip)
+  input_zip = zipfile.ZipFile(input_zip_path, "a")
+  common.ZipWriteStr(input_zip, "updater-script",
+                     "\n".join(script.script) + "\n")
   metadata["ota-required-cache"] = str(script.required_cache)
 
+  common.ZipClose(input_zip)
   # We haven't written the metadata entry, which will be done in
   # FinalizeMetadata.
   common.ZipClose(output_zip)
