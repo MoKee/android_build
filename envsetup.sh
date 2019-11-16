140e67fc7a9d0569bf5239c97e4a7990bbdcf461
Martincz Gao <martincz@mokeedev.com>
2019-11-16 23:19:55 +0800

    build: Fix integer expression expected
    Change-Id: I15a074b01c9c613bf7ac1be10ef038a192c9cd2c

--- envsetup.sh
+++ envsetup.sh
@@ -350,7 +350,7 @@ function set_ccache()
     fi
 
     if [ -z "$CCACHE_SIZE" ]; then
-        if [ "$1" -eq "mokee_default" ]; then
+        if [ "$1" == "mokee_default" ]; then
             CCACHE_SIZE=50G
         else
             CCACHE_SIZE=16G
